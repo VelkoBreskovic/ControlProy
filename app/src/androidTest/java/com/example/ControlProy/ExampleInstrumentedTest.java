c5b59b5f6f579a06ae5c0162465bf2722ab1ec67
neckroangel@gmail.com <Velkyo19??>
2022-09-26 13:02:12 -0300

    Initial commit

--- app/src/androidTest/java/com/example/ControlProy/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/ControlProy/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.miprimeraapp", appContext.getPackageName());
+        assertEquals("com.example.ControlProy", appContext.getPackageName());
     }
 }
--- app/src/main/java/com/example/ControlProy/Loadingpage.java
+++ app/src/main/java/com/example/ControlProy/Loadingpage.java
@@ -1,5 +1,4 @@
 package com.example.ControlProy;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -8,6 +7,8 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ControlProy.R;
 
 public class Loadingpage extends AppCompatActivity {
 
